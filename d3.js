36351f53d43344dd2ec03258909f634c586098ee
codemattcode <codemattcode@gmail.com>
2017-05-21 11:16:00 +0100

    cl

--- d3.js
+++ d3.js
@@ -20,3 +20,28 @@ var canvas = d3.select('body')
     .attr('r', function(d){
       return d;
     });
+
+
+
+    function scaling(){
+        var graphData = [10,400],
+            w         = 400,
+            h         = 400;
+        var canvas = d3.select('.graphContainer')
+        .append('svg')
+        .attr('width', w)
+        .attr('height',h);
+
+        var graphBars = canvas.selectAll('rect')
+        .data(graphData)
+        .enter()
+        .append('rect')
+        .attr('fill', 'blue')
+        .attr('width', function(d){
+            return d;
+        })
+        .attr('height', 20)
+        .attr('y', function(d,i){
+            return i * 50;
+        })
+    }
